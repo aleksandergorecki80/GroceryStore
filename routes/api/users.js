97e041689d0229d1babea770d7a15214bb2fc257
Aleksander <aleksandergorecki80@gmail.com>
2021-08-31 23:16:29 +0100

    Sending a confirmation e-mail

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,9 @@ const User = require('../../models/UserModel');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const jwtSecret = config.get('jwtSecret');
+const emailData = config.get('emailData');
 const bcypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 
 
 // @route   POST api/users
@@ -20,7 +22,7 @@ router.post('/', async (req, res) => {
             name,
             email,
             password
-        })
+        });
         
         // Encrypting the password
         const salt = await bcypt.genSalt(10);
@@ -34,12 +36,52 @@ router.post('/', async (req, res) => {
                 id: user._id
             }
         }
+
+
+
         jwt.sign(payload, jwtSecret,
             { expiresIn: 3600 },
             (err, token) => {
                 if(err) throw err;
+
+                // Sending an confirmation email
+                const url = `http://localhost:3000/confirmation/${token}`
+                const output = `
+                    <p>You have a new contact request</p>
+                    <h3>Contact details:</h3>
+                    <ul>
+                        <li>Name: ${req.body.name}</li>
+                        <li>Email: ${req.body.email}</li>
+                    </ul>
+                    <h3>Message</h3>
+                    <p>Please click the link: <a href="${url}">${url}</a> to confirm your email adress</p>
+                `;
+                // async..await is not allowed in global scope, must use a wrapper
+                async function main() {
+                     // create reusable transporter object using the default SMTP transport
+                    let transporter = nodemailer.createTransport({
+                        host: "smtp.live.com",
+                        port: 587,
+                        secure: false, // true for 465, false for other ports
+                        auth: {
+                        user: emailData.mailUser,
+                        pass: emailData.mailPassword, 
+                        },
+                    });
+                    
+                    // send mail with defined transport object
+                    let info = await transporter.sendMail({
+                        from: `www.aleksander-gorecki.com ${emailData.mailUser}`, // sender address
+                        to: "a.gorecki1980@gmail.com", // list of receivers
+                        subject: "New info from www.grocerystore.com", // Subject line
+                        text: "Hello world?", // plain text body
+                        html: output, // html body
+                    });
+                    console.log("Message sent: %s", info.messageId);
+                }
+                main().catch(console.error);
                 return res.json( { token });
-            });          
+            });
     } catch(err) {
         console.error(err.message);
         return res.status(500).send('Server error');
